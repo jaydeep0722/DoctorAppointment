0dfce7285122ebbb55933a62afc0b5d6a07e6324
jaydeep <jaydeepprajapati243@gmail.com>
2025-07-04 12:08:03 +0530

    Fix: corrected case in Banner import

--- FrontEnd/my-project/src/pages/Home.jsx
+++ FrontEnd/my-project/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "../components/Header";
 import Speciality from "../components/Speciality";
 import Topdoctors from "../components/Topdoctors";
-import Banner from "../components/banner";
+import Banner from "../components/Banner";
 import Footer from "../components/Footer";
 
 const Home = () => {
